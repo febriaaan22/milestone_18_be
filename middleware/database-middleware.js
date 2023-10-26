eb4ba1301f5a66c4c758861b5160b03385675daf
Ireng Febrian Sanjaya <febriansajaya22@gmail.com>
2023-10-26 22:15:55 +0700

    Add files via upload

--- middleware/database-middleware.js
+++ middleware/database-middleware.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 
 const databaseMiddleware = async (req, res, next) => {
 	const mongoClient = await new MongoClient(process.env.MONGO_SIGN).connect();
-	db = mongoClient.db("TaylorSwift");
+	db = mongoClient.db("Todo");
 
 	req.db = db;
 	console.log("success connected to mongodb");
